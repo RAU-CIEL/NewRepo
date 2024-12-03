aa5794199191ecaad5db101d4c965b0e70294444
Philippe Raulet <philipperbreton@gmail.com>
2024-12-03 09:08:42 +0100

    commit 2 # Modification setx et y

--- carre.h
+++ carre.h
@@ -1,4 +1,17 @@
 #pragma once
 
 
-
+class CCarre
+{
+private : 
+	int sx;
+	int sy;
+	unsigned int cote;
+public :
+	void Setsx(int sx1);
+	void Setsy(int sy1);
+	void SetCote(unsigned int cote1);
+	int Getsx();
+	int Getsy();
+	unsigned int
+};
---
+++ mcarre.cpp
@@ -0,0 +1,7 @@
+#include "carre.h"
+
+void CCarre::Setsx(int sx1)
+{
+	this
+
+}
