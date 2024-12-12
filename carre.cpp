33e735d4f9b0ed81df3a80b9b628ef5a10bf858e
Philippe Raulet <philipperbreton@gmail.com>
2024-12-12 10:49:34 +0100

    commit # 4

--- carre.cpp
+++ carre.cpp
@@ -65,7 +65,7 @@ void CCarre::Deplacer(char direction, int saut)
         sx -= saut;
         break;
     case 'e':  // Est
-        // Déplacer vers la 
+        // Déplacer vers la d
         sx += saut;
         break;
     default:
